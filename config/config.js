5b5b6f4b47e6a235734f685c3e22facc1febc2e6
dawah-raka48 <dawah.raka@gmail.com>
2026-07-19 19:29:35 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,14 @@
-// config.js
+/* ==========================================
+   Weekly Reports System
+   Config
+========================================== */
+
+const CONFIG = {
+
+    API_URL: "https://script.google.com/macros/s/AKfycbxqSj0C6Lb52qNTZ1ELzjxKs_J64IntGGe8Ymdc4JVO7TQg9_W_kuI_esJ4YRQdeCwR/exec",
+
+    APP_NAME: "Weekly Reports System",
+
+    VERSION: "1.0.0"
+
+};
