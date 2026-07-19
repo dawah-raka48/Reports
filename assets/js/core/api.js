775c5e669386beb945f55b09c1e026e3dd013732
dawah-raka48 <dawah.raka@gmail.com>
2026-07-19 19:30:25 +0300

    Update api.js

--- assets/js/core/api.js
+++ assets/js/core/api.js
@@ -1,1 +1,51 @@
-// api.js
+/* ==========================================
+   API Service
+========================================== */
+
+class ApiService {
+
+    static async request(action, data = {}) {
+
+        try {
+
+            const response = await fetch(CONFIG.API_URL, {
+
+                method: "POST",
+
+                headers: {
+
+                    "Content-Type": "application/json"
+
+                },
+
+                body: JSON.stringify({
+
+                    action,
+
+                    ...data
+
+                })
+
+            });
+
+            return await response.json();
+
+        }
+
+        catch (error) {
+
+            console.error(error);
+
+            return {
+
+                success: false,
+
+                message: "Network Error"
+
+            };
+
+        }
+
+    }
+
+}
