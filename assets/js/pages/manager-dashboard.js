9807acfabf39aa83ac7ff0b5983e5213ad242ce4
dawah-raka48 <dawah.raka@gmail.com>
2026-07-20 11:19:48 +0300

    Update and rename manager.js to manager-dashboard.js

---
+++ assets/js/pages/manager-dashboard.js
@@ -0,0 +1,188 @@
+/* ==========================================
+   MANAGER DASHBOARD
+========================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    Theme.init();
+
+    checkSession();
+
+    startClock();
+
+    initializeButtons();
+
+    loadManager();
+
+});
+
+
+/* ==========================
+   Session
+========================== */
+
+function checkSession(){
+
+    const session = JSON.parse(
+
+        localStorage.getItem("session")
+
+    );
+
+    if(!session){
+
+        window.location.href="../../index.html";
+
+        return;
+
+    }
+
+    if(session.role!=="manager"){
+
+        window.location.href="../../index.html";
+
+        return;
+
+    }
+
+}
+
+
+/* ==========================
+   Clock
+========================== */
+
+function startClock(){
+
+    updateClock();
+
+    setInterval(updateClock,1000);
+
+}
+
+function updateClock(){
+
+    const now=new Date();
+
+    document.getElementById("clock").textContent=
+
+        now.toLocaleTimeString(
+
+            "ar-SA",
+
+            {
+
+                hour:"2-digit",
+
+                minute:"2-digit",
+
+                second:"2-digit",
+
+                hour12:true
+
+            }
+
+        );
+
+}
+
+
+/* ==========================
+   Buttons
+========================== */
+
+function initializeButtons(){
+
+    document
+
+        .getElementById("logoutBtn")
+
+        .addEventListener(
+
+            "click",
+
+            logout
+
+        );
+
+    document
+
+        .getElementById("employeesBtn")
+
+        .addEventListener(
+
+            "click",
+
+            ()=>{
+
+                alert("سيتم إنشاء صفحة موظفي القسم.");
+
+            }
+
+        );
+
+    document
+
+        .getElementById("reportsBtn")
+
+        .addEventListener(
+
+            "click",
+
+            ()=>{
+
+                alert("سيتم إنشاء صفحة متابعة التقارير.");
+
+            }
+
+        );
+
+}
+
+
+/* ==========================
+   Load Manager
+========================== */
+
+function loadManager(){
+
+    const session=JSON.parse(
+
+        localStorage.getItem("session")
+
+    );
+
+    document.getElementById(
+
+        "managerName"
+
+    ).textContent=
+
+        session.fullName;
+
+    document.getElementById(
+
+        "departmentName"
+
+    ).textContent=
+
+        session.departmentName || "...";
+
+}
+
+
+/* ==========================
+   Logout
+========================== */
+
+function logout(){
+
+    localStorage.removeItem(
+
+        "session"
+
+    );
+
+    window.location.href="../../index.html";
+
+}
