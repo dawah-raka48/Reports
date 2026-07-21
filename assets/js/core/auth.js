59db351ddb6d4d354c04e6d854691758e2620750
dawah-raka48 <dawah.raka@gmail.com>
2026-07-21 20:19:44 +0300

    Update auth.js

--- assets/js/core/auth.js
+++ assets/js/core/auth.js
@@ -16,6 +16,20 @@ const Auth = {
 
         window.location.href = "/";
 
+    },
+
+    getSession() {
+
+        const session = localStorage.getItem("session");
+
+        if (!session) {
+
+            return null;
+
+        }
+
+        return JSON.parse(session);
+
     }
 
 };
