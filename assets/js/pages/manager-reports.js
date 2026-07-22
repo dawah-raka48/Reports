a3575a981aed9d58530da6e0cdf1c620f86a071a
dawah-raka48 <dawah.raka@gmail.com>
2026-07-22 18:05:42 +0300

    Update manager-reports.js

--- assets/js/pages/manager-reports.js
+++ assets/js/pages/manager-reports.js
@@ -121,23 +121,26 @@ function updateClock(){
 function setupEvents(){
 
     document
-
-        .getElementById(
-
-            "logoutBtn"
-
-        )
-
+        .getElementById("logoutBtn")
         .addEventListener(
-
             "click",
-
             ()=>{
 
                 Auth.logout();
 
             }
+        );
 
+    document
+        .getElementById("backBtn")
+        .addEventListener(
+            "click",
+            ()=>{
+
+                window.location.href =
+                    "dashboard.html";
+
+            }
         );
 
 }
